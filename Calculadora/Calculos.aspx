72152dc0aadc50590aad40df8213ef4b821fce9b
Nelson <Nelson@DESKTOP-B1M6VN4>
2023-09-08 16:01:36 -0300

    Agregar archivos de proyecto.

---
+++ Calculadora/Calculos.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculos.aspx.cs" Inherits="Calculadora.Calculos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="1° Numero         "></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="2° Numero   "></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" style="margin-bottom: 0px">
+            <asp:ListItem>Sumar</asp:ListItem>
+            <asp:ListItem>Restar</asp:ListItem>
+            <asp:ListItem>Multiplicar</asp:ListItem>
+            <asp:ListItem>Dividir</asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Calcular" />
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Resultado   "></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server" Width="158px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <br />
+    </form>
+</body>
+</html>
---
+++ Calculadora/Calculos.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.EnterpriseServices.CompensatingResourceManager;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculadora
+{
+    public partial class Calculos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox3.Text = string.Empty;
+            try
+            {
+                if (TextBox1.Text != " " && TextBox2.Text != " ")
+                {
+                    int num1 = int.Parse(TextBox1.Text);
+                    int num2 = int.Parse(TextBox2.Text);
+                    float resultado = 0;
+                    if (RadioButtonList1.SelectedItem.Value == "Sumar")
+                    {
+                        resultado = num1 + num2;
+                        TextBox3.Text = Convert.ToString(resultado);
+                    }
+                    else if (RadioButtonList1.SelectedItem.Value == "Restar")
+                    {
+                        resultado = num1 - num2;
+                        TextBox3.Text = Convert.ToString(resultado);
+                    }
+                    else if (RadioButtonList1.SelectedItem.Value == "Multiplicar")
+                    {
+                        resultado = num1 * num2;
+                        TextBox3.Text = Convert.ToString(resultado);
+                    }
+                    else if (RadioButtonList1.SelectedItem.Value == "Dividir")
+                    {
+                        resultado = num1 / num2;
+                        TextBox3.Text = Convert.ToString(resultado);
+                    }
+                }
+
+            }
+            catch
+            {
+                TextBox3.Text = "Debe completar los datos ";
+            }
+
+
+
+
+        }
+    }
+}
